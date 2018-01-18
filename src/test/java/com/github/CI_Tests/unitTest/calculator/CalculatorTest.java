fe83ba906a77b310ec259b9dcaf0aa0a235ac5b1
ChanceTom <thomas.coupechoux.73@hotmail.fr>
2018-01-18 09:24:42 +0100

    Ajout Test

--- src/test/java/com/github/CI_Tests/unitTest/calculator/CalculatorTest.java
+++ src/test/java/com/github/CI_Tests/unitTest/calculator/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.github.CI_Tests.unitTest.calculator;
+﻿package com.github.CI_Tests.unitTest.calculator;
 
 import junit.framework.Assert;
 import org.junit.Test;
@@ -13,6 +13,45 @@ public class CalculatorTest {
 		int result = calculator.sum(2, 2);
 		// Then
 		if (result != 4) {   // if 2 + 2 != 4
+			Assert.fail();
+		}
+	}
+
+
+	@Test
+	public void testSub() {
+		// Given
+		Calculator calculator = new Calculator();
+		// When
+		int result = calculator.sub(2, 2);
+		// Then
+		if (result != 0) {   // if 2 - 2 != 0
+			Assert.fail();
+		}
+	}
+
+
+	@Test
+	public void testTimes() {
+		// Given
+		Calculator calculator = new Calculator();
+		// When
+		int result = calculator.times(2, 2);
+		// Then
+		if (result != 4) {   // if 2 * 2 != 4
+			Assert.fail();
+		}
+	}
+
+
+	@Test
+	public void testDiv() {
+		// Given
+		Calculator calculator = new Calculator();
+		// When
+		int result = calculator.div(2, 2);
+		// Then
+		if (result != 1) {   // if 2 / 2 != 1
 			Assert.fail();
 		}
 	}
