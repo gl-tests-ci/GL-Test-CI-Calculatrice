a69fac3316997bdc42fba05e0f7939d20606acbf
ChanceTom <thomas.coupechoux.73@hotmail.fr>
2018-01-18 09:25:17 +0100

    Ajout Test

--- src/main/java/com/github/CI_Tests/unitTest/calculator/Calculator.java
+++ src/main/java/com/github/CI_Tests/unitTest/calculator/Calculator.java
@@ -1,11 +1,20 @@
-package com.github.CI_Tests.unitTest.calculator;
+﻿package com.github.CI_Tests.unitTest.calculator;
 
 public class Calculator {
 
 	public int sum(int a, int b) {
 		return a + b;
 	}
-
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int times(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
 	/**
 	*	A vous de développer la suite !
 	*/
